97aacf09ed1fa5577478d9c812003f9a72c45f96
Jakub Adamczyk <adamjaku@charon.kis.agh.edu.pl>
2016-12-12 11:45:16 +0100

    comment the code

--- lista.py
+++ lista.py
@@ -4,5 +4,43 @@ Created on Mon Dec 12 11:20:29 2016
 
 @author: adamjaku
 """
-
-a
+class Node:
+ 
+    def __init__(self, data):
+        self.data = data#nalezy dodac potemdodatkowe pole
+        self.next = None
+        self.prev = None
+ 
+    def __str__(self):
+        return str(self.data)
+ 
+class UnidirectionalList:
+ 
+    def __init__(self):
+        self.head = None
+        self.size = 0
+ 
+    def addLast(self, data):    #dodaje element na koncu listy
+        if not self.head:
+            n = Node(data)
+            self.head = n
+        else:
+            n = self.head
+            while n.next != None:
+                n = n.next
+            new_node = Node(data)
+            n.next = new_node;
+ 
+    def printList(self):
+        n = self.head
+        while n:
+            print (n)
+            n = n.next
+ 
+ll = UnidirectionalList()
+ll.addLast(14)
+ll.addLast("test")
+ll.addLast(2.34)
+ll.addLast(True)
+ 
+ll.printList()
